95e56fde8a1ac7bab4d6edc6e14e10e751dee511
Jujhar Singh <jujhar@jujhar.com>
2023-07-25 22:14:33 +0100

    experiment

--- pkg/scrape/scrape.go
+++ pkg/scrape/scrape.go
@@ -3,6 +3,7 @@ package scrape
 import (
 	"fmt"
 	"log"
+	"time"
 
 	// importing Colly
 	"github.com/gocolly/colly"
@@ -15,24 +16,41 @@ type hukam struct {
 }
 
 func GetTodaysHukamnama(url string) hukam {
-	result := hukam{"2023-07-23 04:35:00",
-		597,
-		"ਮੇਰੈ ਹੀਅਰੈ ਰਤਨੁ ਨਾਮੁ ਹਰਿ ਬਸਿਆ ਗੁਰਿ ਹਾਥੁ ਧਰਿਓ ਮੇਰੈ ਮਾਥਾ ॥ ਜਨਮ ਜਨਮ ਕੇ ਕਿਲਬਿਖ ਦੁਖ ਉਤਰੇ ਗੁਰਿ ਨਾਮੁ ਦੀਓ ਰਿਨੁ ਲਾਥਾ ॥੧॥ ਮੇਰੇ ਮਨ ਭਜੁ ਰਾਮ ਨਾਮੁ ਸਭਿ ਅਰਥਾ ॥ ਗੁਰਿ ਪੂਰੈ ਹਰਿ ਨਾਮੁ ਦ੍ਰਿੜਾਇਆ ਬਿਨੁ ਨਾਵੈ ਜੀਵਨੁ ਬਿਰਥਾ ॥ ਰਹਾਉ ॥ ਬਿਨੁ ਗੁਰ ਮੂੜ ਭਏ ਹੈ ਮਨਮੁਖ ਤੇ ਮੋਹ ਮਾਇਆ ਨਿਤ ਫਾਥਾ ॥ ਤਿਨ ਸਾਧੂ ਚਰਣ ਨ ਸੇਵੇ ਕਬਹੂ ਤਿਨ ਸਭੁ ਜਨਮੁ ਅਕਾਥਾ ॥੨॥ ਜਿਨ ਸਾਧੂ ਚਰਣ ਸਾਧ ਪਗ ਸੇਵੇ ਤਿਨ ਸਫਲਿਓ ਜਨਮੁ ਸਨਾਥਾ ॥ ਮੋ ਕਉ ਕੀਜੈ ਦਾਸੁ ਦਾਸ ਦਾਸਨ ਕੋ ਹਰਿ ਦਇਆ ਧਾਰਿ ਜਗੰਨਾਥਾ ॥੩॥ ਹਮ ਅੰਧੁਲੇ ਗਿਆਨਹੀਨ ਅਗਿਆਨੀ ਕਿਉ ਚਾਲਹ ਮਾਰਗਿ ਪੰਥਾ ॥ ਹਮ ਅੰਧੁਲੇ ਕਉ ਗੁਰ ਅੰਚਲੁ ਦੀਜੈ ਜਨ ਨਾਨਕ ਚਲਹ ਮਿਲੰਥਾ ॥੪॥੧॥"}
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("old.sgpc.net", "localhost"),
+	// colly.Async(true), colly.AllowedDomains("old.sgpc.net", "localhost"),
 	)
-	// Find and print all links
-	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-		links := e.ChildAttrs("a", "href")
-		fmt.Println(links)
+
+	c.SetRequestTimeout(120 * time.Second)
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Got a response from", r.Request.URL)
 	})
 
 	c.OnError(func(_ *colly.Response, err error) {
-		log.Fatalf("Something went wrong getting response:", err)
+		log.Fatalln("Something went wrong getting response", err)
+	})
+
+	result := hukam{}
+
+	// Find and print all links
+	c.OnHTML("", func(e *colly.HTMLElement) {
+		links := e.ChildAttrs("a", "href")
+		fmt.Println(links)
+		e.Request.Visit(e.Attr("href"))
+
+		result.ang = 597
+		result.gurmukhi = "ਮੇਰੈ ਹੀਅਰੈ ਰਤਨੁ ਨਾਮੁ ਹਰਿ ਬਸਿਆ ਗੁਰਿ ਹਾਥੁ ਧਰਿਓ ਮੇਰੈ ਮਾਥਾ ॥ ਜਨਮ ਜਨਮ ਕੇ ਕਿਲਬਿਖ ਦੁਖ ਉਤਰੇ ਗੁਰਿ ਨਾਮੁ ਦੀਓ ਰਿਨੁ ਲਾਥਾ ॥੧॥ ਮੇਰੇ ਮਨ ਭਜੁ ਰਾਮ ਨਾਮੁ ਸਭਿ ਅਰਥਾ ॥ ਗੁਰਿ ਪੂਰੈ ਹਰਿ ਨਾਮੁ ਦ੍ਰਿੜਾਇਆ ਬਿਨੁ ਨਾਵੈ ਜੀਵਨੁ ਬਿਰਥਾ ॥ ਰਹਾਉ ॥ ਬਿਨੁ ਗੁਰ ਮੂੜ ਭਏ ਹੈ ਮਨਮੁਖ ਤੇ ਮੋਹ ਮਾਇਆ ਨਿਤ ਫਾਥਾ ॥ ਤਿਨ ਸਾਧੂ ਚਰਣ ਨ ਸੇਵੇ ਕਬਹੂ ਤਿਨ ਸਭੁ ਜਨਮੁ ਅਕਾਥਾ ॥੨॥ ਜਿਨ ਸਾਧੂ ਚਰਣ ਸਾਧ ਪਗ ਸੇਵੇ ਤਿਨ ਸਫਲਿਓ ਜਨਮੁ ਸਨਾਥਾ ॥ ਮੋ ਕਉ ਕੀਜੈ ਦਾਸੁ ਦਾਸ ਦਾਸਨ ਕੋ ਹਰਿ ਦਇਆ ਧਾਰਿ ਜਗੰਨਾਥਾ ॥੩॥ ਹਮ ਅੰਧੁਲੇ ਗਿਆਨਹੀਨ ਅਗਿਆਨੀ ਕਿਉ ਚਾਲਹ ਮਾਰਗਿ ਪੰਥਾ ॥ ਹਮ ਅੰਧੁਲੇ ਕਉ ਗੁਰ ਅੰਚਲੁ ਦੀਜੈ ਜਨ ਨਾਨਕ ਚਲਹ ਮਿਲੰਥਾ ॥੪॥੧॥"
+		result.dateTime = "2023-07-23 04:35:00"
 	})
 
 	c.Visit(url)
 
+	c.Wait()
+
 	return result
+
 }
--- pkg/scrape/scrape_test.go
+++ pkg/scrape/scrape_test.go
@@ -28,6 +28,10 @@ var testData = []testCase{
 		"2023-07-23 04:35:00",
 		"ਮੇਰੈ ਹੀਅਰੈ ਰਤਨੁ ਨਾਮੁ ਹਰਿ ਬਸਿਆ ਗੁਰਿ ਹਾਥੁ ਧਰਿਓ ਮੇਰੈ ਮਾਥਾ ॥ ਜਨਮ ਜਨਮ ਕੇ ਕਿਲਬਿਖ ਦੁਖ ਉਤਰੇ ਗੁਰਿ ਨਾਮੁ ਦੀਓ ਰਿਨੁ ਲਾਥਾ ॥੧॥ ਮੇਰੇ ਮਨ ਭਜੁ ਰਾਮ ਨਾਮੁ ਸਭਿ ਅਰਥਾ ॥ ਗੁਰਿ ਪੂਰੈ ਹਰਿ ਨਾਮੁ ਦ੍ਰਿੜਾਇਆ ਬਿਨੁ ਨਾਵੈ ਜੀਵਨੁ ਬਿਰਥਾ ॥ ਰਹਾਉ ॥ ਬਿਨੁ ਗੁਰ ਮੂੜ ਭਏ ਹੈ ਮਨਮੁਖ ਤੇ ਮੋਹ ਮਾਇਆ ਨਿਤ ਫਾਥਾ ॥ ਤਿਨ ਸਾਧੂ ਚਰਣ ਨ ਸੇਵੇ ਕਬਹੂ ਤਿਨ ਸਭੁ ਜਨਮੁ ਅਕਾਥਾ ॥੨॥ ਜਿਨ ਸਾਧੂ ਚਰਣ ਸਾਧ ਪਗ ਸੇਵੇ ਤਿਨ ਸਫਲਿਓ ਜਨਮੁ ਸਨਾਥਾ ॥ ਮੋ ਕਉ ਕੀਜੈ ਦਾਸੁ ਦਾਸ ਦਾਸਨ ਕੋ ਹਰਿ ਦਇਆ ਧਾਰਿ ਜਗੰਨਾਥਾ ॥੩॥ ਹਮ ਅੰਧੁਲੇ ਗਿਆਨਹੀਨ ਅਗਿਆਨੀ ਕਿਉ ਚਾਲਹ ਮਾਰਗਿ ਪੰਥਾ ॥ ਹਮ ਅੰਧੁਲੇ ਕਉ ਗੁਰ ਅੰਚਲੁ ਦੀਜੈ ਜਨ ਨਾਨਕ ਚਲਹ ਮਿਲੰਥਾ ॥੪॥੧॥",
 		696},
+	// {"2023-07-25.html",
+	// 	"2023-07-25 04:35:00",
+	// 	"hir isau pRIiq AMqru mnu byiDAw hir ibnu rhxu n jweI ] ijau mCulI ibnu nIrY ibnsY iqau nwmY ibnu mir jweI ]1] myry pRB ikrpw jlu dyvhu hir nweI ] hau AMqir nwmu mMgw idnu rwqI nwmy hI sWiq pweI ] rhwau ] ijau cwiqRku jl ibnu ibllwvY ibnu jl ipAws n jweI ] gurmuiK jlu pwvY suK shjy hirAw Bwie suBweI ]2] mnmuK BUKy dh ids folih ibnu nwvY duKu pweI ] jnim mrY iPir jonI AwvY drgh imlY sjweI ]3] ikRpw krih qw hir gux gwvh hir rsu AMqir pweI ] nwnk dIn dieAwl Bey hY iqRsnw sbid buJweI ]4]8]",
+	// 	607},
 }
 
 // Ensure that a webserver is running on 8081 with some test files
@@ -38,8 +42,12 @@ func TestHukamnama(t *testing.T) {
 	for _, wants := range testData {
 		hukam := GetTodaysHukamnama(fmt.Sprintf("http://localhost:8081/%s", wants.filename))
 
+		fmt.Println(hukam)
 		if hukam.dateTime != wants.dateTime {
 			t.Fatalf("Hukamnama date is wrong, wants %s got %s,", hukam.dateTime, wants.dateTime)
+		}
+		if hukam.ang != wants.ang {
+			t.Fatalf("Hukamnama ang is wrong, wants %d got %d,", hukam.ang, wants.ang)
 		}
 		if hukam.gurmukhi != wants.gurmukhi {
 			t.Fatalf("Hukamnama Gurmukhi is wrong, wants %s got %s,", hukam.gurmukhi, wants.gurmukhi)
